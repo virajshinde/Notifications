51bfa8add029c6314ce3e2d98fac57788a4fc91d
Viraj Shinde <virajabhayshinde@gmail.com>
2021-07-30 11:29:01 +0530

    Imperfect notification and deleteion of appointment working.

--- src/components/notifications.js
+++ src/components/notifications.js
@@ -5,8 +5,8 @@ import {useState} from 'react'
 
 
 
-const notify1 = new Date(2021,7,29,19,11).getMinutes()
-const notifystring1 = notify1.toString()
+// const notify1 = new Date(2021,7,30,8,16).getMinutes()
+// const notifystring1 = notify1.toString()
 // const notify2 = new Date(2021,7,29,18,51)
 // const notifystring2 = notify2.toString()
 
@@ -19,27 +19,44 @@ const dateNotifyString = dateNotify.toString()
 const Notify = () => {
 
     var notify = null
-    const notify2 = new Date().getMinutes()
+    const notify1 = new Date(2021,7,30,9,55).getMinutes()
+const notifystring1 = notify1.toString()
+    const notify2 = new Date(2021,7,30,11,28 ).getMinutes()
     const notifystring2 = notify2.toString()
-    const [dates,setDates] = useState([notifystring1,notifystring2])
+    const notifyCurrent = new Date().getMinutes()
+    const currentDate = notifyCurrent.toString()    
+
+    const [Dates,setDates] = useState([notifystring1,notifystring2])
     const [isActive,setIsActive] = useState(true)
         
      const notification = null
     
-    setInterval(() => {
-        console.log(dates)
-     if(dates[0]===dates[1]){
-         notify = 'Norified'
-     }else{
-         notify = 'Not notified'
-     }    
-    })
-    console.log(dates)
-     if(dates[0]===dates[1]){
-         notify = 'Norified'
-     }else{
-         notify = 'Not notified'
-     }
+    
+    const datesList = Dates.map((el)=> {
+            if(currentDate=== el ){
+                notify = 'Norified'
+                var indexDel = Dates.indexOf(el);
+                Dates.splice(indexDel,1)
+                // var updateDates = Dates.map(() => {
+                console.log(Dates)
+                // })
+                // setDates(updateDates)
+                
+            }else{
+                notify = 'Not notified'
+            }
+            return notify
+        })
+        console.log()
+        var renderedOutput = Dates.map(item => <div> {item} </div>)
+         console.log(renderedOutput)
+    
+    // console.log(dates)
+    //  if(dates[0]===dates[1]){
+    //      notify = 'Norified'
+    //  }else{
+    //      notify = 'Not notified'
+    //  }
     
     const handleShow = ()=>{
         setIsActive(true)
@@ -55,10 +72,9 @@ const Notify = () => {
         <div>
             <div className={"container"}>
 
-            {isActive && <>
-            <div>{notifystring1}</div>
-            <div>{notifystring2}</div>
-            </>
+            {isActive && 
+                Dates.map(item => <div> {item} </div>)
+            
             }
             
             <button onClick = {handleShow}>Show</button>
@@ -67,7 +83,7 @@ const Notify = () => {
 
             <div>
                 
-               {notify} 
+               {datesList} 
                 
             </div>
             
